80246eb0ff306d6881549f9255522c29b1edbe1e
Rishi Musuvathi <rishimusuvathi03@gmail.com>
2023-10-21 17:47:34 -0400

    first commit for Arjun

---
+++ Main.js
@@ -0,0 +1,7 @@
+// Collect refereneces from the html file
+const submitButton = document.getElementById("");
+const trackButton = document.getElementById("");
+const trackIron = document.getElementById("");
+
+// handle Buttons
+submitButton.addEventListener("click", function () {});
