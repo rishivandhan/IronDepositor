bbab220544fd558fdfe8f1b95101e65e1aee99ea
Rishi Musuvathi <rishimusuvathi03@gmail.com>
2023-10-21 19:35:56 -0400

    finalized

--- Main.js
+++
@@ -1,7 +0,0 @@
-// Collect refereneces from the html file
-const submitButton = document.getElementById("");
-const trackButton = document.getElementById("");
-const trackIron = document.getElementById("");
-
-// handle Buttons
-submitButton.addEventListener("click", function () {});
